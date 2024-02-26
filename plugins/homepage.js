f117c781e57046fff19f4079323b02f138deb254
Paul <imsbt@outlook.com>
2024-02-26 14:48:56 +0800

    modify

--- plugins/homepage.js
+++ plugins/homepage.js
@@ -1,38 +1,36 @@
 import { createPage } from '@vuepress/core';
-import {getDirectory} from '../utils';
-
-
+import { getDirectory } from '../utils';
 
 export default {
-    name: "vuepress-plugin-default-homepage",
-    // 初始化之后，所有的页面已经加载完毕
-    async onInitialized(app) {
-        const allDirectory = getDirectory(app.options.source);
-        let content = '';
-        allDirectory.forEach(item => {
-            content += `[${item.name}](./${item.name})\n\n`
-        })
+  name: 'vuepress-plugin-default-homepage',
+  // 初始化之后，所有的页面已经加载完毕
+  async onInitialized(app) {
+    const allDirectory = getDirectory(app.options.source);
+    console.log(allDirectory);
+    let content = '';
+    allDirectory.forEach((item) => {
+      content += `[${item.name}](./${item.name})\n\n`;
+    });
 
-
-        // 如果主页不存在
-        if (app.pages.every((page) => page.path !== '/')) {
-            // 创建一个主页
-            const homepage = await createPage(app, {
-                path: '/',
-                Key: "主页",
-                // 设置 frontmatter
-                frontmatter: {
-                    layout: 'Layout',
-                },
-                // 设置 markdown 内容
-                content: `\
+    // 如果主页不存在
+    if (app.pages.every((page) => page.path !== '/')) {
+      // 创建一个主页
+      const homepage = await createPage(app, {
+        path: '/',
+        Key: '主页',
+        // 设置 frontmatter
+        frontmatter: {
+          layout: 'Layout',
+        },
+        // 设置 markdown 内容
+        content: `\
 # 欢迎来到 ${app.options.title}
 
 ${content}
 `,
-            })
-            // 把它添加到 `app.pages`
-            app.pages.push(homepage)
-        }
-    },
-}
+      });
+      // 把它添加到 `app.pages`
+      app.pages.push(homepage);
+    }
+  },
+};

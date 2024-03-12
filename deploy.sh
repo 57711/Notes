f34fe4c364d14032dd014849e1e5fb5092cc2578
Paul <imsbt@outlook.com>
2024-03-12 18:10:24 +0800

    modified

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git config --global user.email "imsbt@outlook.com"
+git config --global user.name "paul"
+
 git push -f git@github.com:57711/Notes.git master:dist-pages
 
 cd -
